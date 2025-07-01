177fbfc7f7fd7008f55b4090cbe25e189240b46e
leo.shi <554601845@qq.com>
2025-07-01 20:28:26 +0800

    update walletconnect

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = { ...config.resolve.fallback, "pino-pretty": false };
-        }
-        return config;
-    },
+    
     async rewrites() {
         return [
             {
