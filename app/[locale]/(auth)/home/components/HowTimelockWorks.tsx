f000044c1b3ed5052ec567d5a4a1f7d71127c8c6
0xkaso <kaso@desyn.io>
2025-07-26 18:25:16 +0800

    fix shadow card

--- app/[locale]/(auth)/home/components/HowTimelockWorks.tsx
+++ app/[locale]/(auth)/home/components/HowTimelockWorks.tsx
@@ -4,7 +4,7 @@ const HowTimelockWorks: React.FC = () => {
   return (
     // Initial state: white background, light border, gray text
     // Hover state (group-hover): black background, white text, blue border (or transparent/dark)
-    <div className="bg-[#F5F5F5] p-8 rounded-xl shadow-md border border-gray-200 group hover:bg-black hover:shadow-lg hover:border-black transition-all duration-300 cursor-pointer">
+    <div className=" p-8 rounded-xl  border border-gray-200 group hover:bg-black hover:shadow-lg hover:border-black transition-all duration-300 cursor-pointer">
       <h3 className="text-lg font-semibold text-gray-800 mb-4 flex items-center group-hover:text-white transition-colors duration-300">
         <span className="text-black mr-2 group-hover:text-white transition-colors duration-300">&gt;</span> Timelock 是如何运行的?
       </h3>
@@ -13,7 +13,7 @@ const HowTimelockWorks: React.FC = () => {
       </p>
       <a
         href="https://timelock.gitbook.io/timelock/"
-        className="inline-flex items-center justify-center bg-white text-black py-2 px-4 rounded-md text-sm font-semibold border border-gray-300 hover:shadow-md /* Add shadow on hover for the button itself */ focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-all duration-300"
+        className="inline-flex items-center justify-center bg-white text-black py-2 px-4 rounded-md text-sm font-semibold border border-gray-300 hover: /* Add shadow on hover for the button itself */ focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-all duration-300"
       >
         Read more{' '}
         <svg
