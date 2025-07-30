d15ecd59a63e9a731f9783aee1b078558b6dc576
0xkaso <kaso@desyn.io>
2025-07-30 23:13:05 +0800

    fix flex

--- app/[locale]/(auth)/create-transaction/page.tsx
+++ app/[locale]/(auth)/create-transaction/page.tsx
@@ -149,7 +149,7 @@ const TransactionEncoderPage: React.FC = () => {
 
     return (
         <PageLayout title={t('title')}>
-            <div className="min-h-screen bg-withe p-8">
+            <div className="min-h-screen bg-withe">
                 <div className="mx-auto flex flex-col"> {/* Main vertical stack of major blocks */}
                     {/* FIRST MAJOR BLOCK: Encoding Transaction Form (Full Width) */}
                     <div className='flex justify-between'>
