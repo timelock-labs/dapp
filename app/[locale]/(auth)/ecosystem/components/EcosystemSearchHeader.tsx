2b24a6e8138e575a32c3e5e995bfd6a293bc31d2
0xkaso <kaso@desyn.io>
2025-07-31 15:13:11 +0800

    add bg coloir

--- app/[locale]/(auth)/ecosystem/components/EcosystemSearchHeader.tsx
+++ app/[locale]/(auth)/ecosystem/components/EcosystemSearchHeader.tsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import Image from 'next/image';
 
+import bg_png from "../images/bg.png"; // Adjust the path as necessary
+
 const EcosystemSearchHeader: React.FC = () => {
   return (
-    <div className="bg-black text-white p-6 rounded-lg shadow-md flex items-center space-x-4 pb-[112px]">
-      <Image 
-        src="/ecoPanter.png" 
-        alt="Ecosystem Partner" 
-        width={20} 
-        height={20} 
+    <div className="bg-black text-white p-6 min-h-[180px] rounded-lg shadow-md flex items-center space-x-4" style={{ backgroundImage: `url(${bg_png.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <Image
+        src="/ecoPanter.png"
+        alt="Ecosystem Partner"
+        width={20}
+        height={20}
         className="text-gray-400"
       />
       <h2 className="text-xl font-semibold">探索我们的生态伙伴</h2>
