1c5def4543c5d864833b0b91e6e390c3815fc722
0xkaso <kaso@desyn.io>
2025-08-20 18:18:29 +0800

    data format

--- utils/utils.ts
+++ utils/utils.ts
@@ -51,7 +51,7 @@ export const formatDate = (dateString: string) => {
 		const minutes = date.getMinutes().toString().padStart(2, '0');
 		const seconds = date.getSeconds().toString().padStart(2, '0');
 
-		return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+		return `${month}/${day} ${hours}:${minutes}:${seconds}`;
 	} catch (error) {
 		console.error('Error formatting date:', error);
 		return dateString;
