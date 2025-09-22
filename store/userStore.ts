80276a9b3de1d0068987aba06b62633ace16947a
0xkaso <kaso@desyn.io>
2025-09-22 14:58:19 +0800

    update baseurl

--- store/userStore.ts
+++ store/userStore.ts
@@ -84,7 +84,7 @@ export const useAuthStore = create<AppState & AppActions>()(
 				}
 
 				try {
-					const response = await fetch('/api/v1/auth/refresh', {
+					const response = await fetch(`${baseURL}/api/v1/auth/refresh`, {
 						method: 'POST',
 						headers: {
 							'Content-Type': 'application/json',
