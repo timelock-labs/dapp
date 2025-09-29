1e414382651bfa3a8e0551d161a5db02133a02ac
0xkaso <kaso@desyn.io>
2025-09-29 20:23:09 +0800

    hidden footer label

--- components/nav/app-sidebar.tsx
+++ components/nav/app-sidebar.tsx
@@ -103,7 +103,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 			<div className='flex flex-col gap-8 justify-center items-center'>
 				{state === 'expanded' && <LanguageSwitcher />}
 				<SidebarFooter className='w-full flex items-center justify-between px-4 py-2'>
-					<a className='' href="">Feedback</a>
+					{/* <a className='' href="">Feedback</a> */}
 					{/* <NavUser
 						user={{
 							name: 'support@timelock.com',
